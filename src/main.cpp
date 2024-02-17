f2d98f4cea789596ac29a307d25e3a3c4a6a9d70
Martin <martin.roger@me.com>
2024-02-17 22:28:50 +0100

    Working initial version

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,28 @@
- #include <Arduino.h>
+#include <Arduino.h>
+#include <SPI.h>
+#include <TFT_eSPI.h>
 
+TFT_eSPI tft = TFT_eSPI(TFT_WIDTH,TFT_HEIGHT);
 // put function declarations here:
 int myFunction(int, int);
 
 void setup() {
   // put your setup code here, to run once:
   int result = myFunction(2, 3);
+  pinMode(TFT_BL,OUTPUT);
+  digitalWrite(TFT_BL,HIGH);
+  tft.begin();
+  tft.fillScreen(TFT_WHITE);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  tft.fillScreen(TFT_RED);
+  delay(1000);
+  tft.fillScreen(TFT_BLUE);
+  delay(1000);
+  tft.fillScreen(TFT_GREEN);
+  delay(1000);
 }
 
 // put function definitions here:
