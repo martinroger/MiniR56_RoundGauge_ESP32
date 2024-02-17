643e158be4f1252a9cfe94c662ef7bfb15fe531f
Martin <martin.roger@me.com>
2024-02-17 23:19:24 +0100

    Clean up

--- src/main.cpp
+++ src/main.cpp
@@ -4,16 +4,28 @@
 #include "CST816S.h"
 
 TFT_eSPI tft = TFT_eSPI(TFT_WIDTH,TFT_HEIGHT);
-// put function declarations here:
-int myFunction(int, int);
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+
   pinMode(TFT_BL,OUTPUT);
   digitalWrite(TFT_BL,HIGH);
   tft.begin();
   tft.fillScreen(TFT_WHITE);
+
+  //These GPIOs are available
+  pinMode(15,OUTPUT);
+  digitalWrite(15,HIGH);
+  pinMode(16,OUTPUT);
+  digitalWrite(16,LOW);
+  pinMode(17,OUTPUT);
+  digitalWrite(17,HIGH);
+  pinMode(18,OUTPUT);
+  digitalWrite(18,LOW);
+  pinMode(21,OUTPUT);
+  digitalWrite(21,HIGH);
+  pinMode(33,OUTPUT);
+  digitalWrite(33,LOW);
 }
 
 void loop() {
@@ -24,9 +36,4 @@ void loop() {
   delay(1000);
   tft.fillScreen(TFT_GREEN);
   delay(1000);
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
 }
