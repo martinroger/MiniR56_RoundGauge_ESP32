361e10fee6ccb4851aa7e1b037335e2a96940751
APF-MRoger <112392920+APF-MRoger@users.noreply.github.com>
2024-09-06 09:00:00 +0200

    Remanning
    Changing approach slightly

--- include/kwp.h
+++ include/kwp.h
@@ -1,16 +1,14 @@
-class kwp
+#pragma once
+#include "Arduino.h"
+#include "kwpFrame.h"
+
+#define CAN_TX 16
+#define CAN_RX 15
+
+enum SIDReq : byte
 {
-private:
-    /* data */
-public:
-    kwp(/* args */);
-    ~kwp();
+    _$23_readMemoryByAddress = 0x23
+    //Others
 };
 
-kwp::kwp(/* args */)
-{
-}
-
-kwp::~kwp()
-{
-}
+//TODO : also add SIDResp enums, frameType enums
---
+++ include/kwpFrame.h
@@ -0,0 +1,22 @@
+#pragma once
+#include "Arduino.h"
+#include <ESP32-TWAI-CAN.hpp>
+
+
+
+class kwpFrame
+{
+private:
+    /* data */
+public:
+    kwpFrame(/* args */);
+    ~kwpFrame();
+};
+
+kwpFrame::kwpFrame(/* args */)
+{
+}
+
+kwpFrame::~kwpFrame()
+{
+}
