0c27aea31042a9e36397ade8a07c45a76511b560
Martin <martin.pepe.roger@gmail.com>
2024-09-07 14:54:09 +0200

    Utils and base DIDs

--- include/DID.h
+++ include/DID.h
@@ -6,26 +6,68 @@
 
 struct DID
 {
-//private:
-    /* data */
-//public:
-    //DID(/* args */);
-    //~DID();
-    byte identifier[2]  =   {0x00};
-    byte memorySize     =   1;
-    byte position       =   1;
-    int32_t mul         =   1;
-    int32_t div         =   1;
-    int32_t add         =   0;
-    //different value type ?
+    byte identifier[2] ;
+    byte memorySize    ;
+    byte position      ;
+    int32_t mul        ;
+    int32_t div        ;
+    int32_t add        ;
 };
-
-
-
-// DID::DID(/* args */)
-// {
-// }
-
-// DID::~DID()
-// {
-// }
+//Known DIDs
+DID _4402_oilTemperature = {
+    .identifier =   {0x44,0x02},
+    .memorySize =   1,
+    .position   =   1,
+    .mul        =   1,
+    .div        =   1,
+    .add        =   -60
+};
+DID _5805_engineTemp_1 = {
+    .identifier =   {0x58,0x05},
+    .memorySize =   1,
+    .position   =   1,
+    .mul        =   3,
+    .div        =   4,
+    .add        =   -48
+};
+DID _580C_RPM = {
+    .identifier =   {0x58,0x0C},
+    .memorySize =   1,
+    .position   =   1,
+    .mul        =   40,
+    .div        =   1,
+    .add        =   0
+};
+DID _580D_speed = {
+    .identifier =   {0x58,0x0D},
+    .memorySize =   1,
+    .position   =   1,
+    .mul        =   5,
+    .div        =   4,
+    .add        =   0
+};
+DID _580F_IAT = {
+    .identifier =   {0x58,0x0F},
+    .memorySize =   1,
+    .position   =   1,
+    .mul        =   3,
+    .div        =   4,
+    .add        =   -48
+};
+DID _58DD_drosselKlappePressure = {
+    .identifier =   {0x58,0xDD},
+    .memorySize =   2,
+    .position   =   1,
+    .mul        =   5,
+    .div        =   64,
+    .add        =   0
+};
+DID _58F0_HPFPPressure = {
+    .identifier =   {0x58,0xF0},
+    .memorySize =   2,
+    .position   =   1,
+    .mul        =   1,
+    .div        =   2000,
+    .add        =   0
+};
+//DIDs to be tested
--- include/kwp.h
+++ include/kwp.h
@@ -3,8 +3,35 @@
 #include "kwpFrame.h"
 #include "DID.h"
 
+//CAN interface management
 #define CAN_TX 16
 #define CAN_RX 15
+
+//Utils
+template <typename T>
+T swap_endian(T u)
+{
+    static_assert (CHAR_BIT == 8, "CHAR_BIT != 8");
+
+    union
+    {
+        T u;
+        unsigned char u8[sizeof(T)];
+    } source, dest;
+
+    source.u = u;
+
+    for (size_t k = 0; k < sizeof(T); k++)
+        dest.u8[k] = source.u8[sizeof(T) - k - 1];
+
+    return dest.u;
+}
+
+/*Usage : 
+tempTrackIndex = swap_endian<uint32_t>(*((uint32_t*)&byteArray[3]));
+*/
+
+//ENUMS
 
 enum SIDReq : byte
 {
