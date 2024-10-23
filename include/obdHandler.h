789f5ba5a3a07640f09f812f68121a03002d8757
Martin <martin.roger@me.com>
2024-10-23 21:33:19 +0200

    Start LVGL 9 port migration

--- include/obdHandler.h
+++ include/obdHandler.h
@@ -1,7 +1,10 @@
 #include <ESP32-TWAI-CAN.hpp>
-
-#define CAN_TX 16
-#define CAN_RX 15
+#ifndef CAN_TX
+	#define CAN_TX 16
+#endif
+#ifndef CAN_RX
+	#define CAN_RX 15
+#endif
 
 CanFrame rxFrame = { 0 };
 
@@ -22,24 +25,24 @@ unsigned char byteE;
 byte requestID = 0x05;
 
 void sendObdFrame(uint8_t obdId) {
-    CanFrame obdFrame = { 0 };
-    obdFrame.identifier = 0x7DF; // Default OBD tester address;
-    obdFrame.extd = 0;
-    obdFrame.data_length_code = 8;
-    obdFrame.data[0] = 0x02;
-    obdFrame.data[1] = 0x01;    // Mode 1
-    obdFrame.data[2] = obdId;
-    obdFrame.data[3] = 0xAA;    // Best to use 0xAA (0b10101010) instead of 0
-    obdFrame.data[4] = 0xAA;    // CAN works better this way as it needs
-    obdFrame.data[5] = 0xAA;    // to avoid bit-stuffing
-    obdFrame.data[6] = 0xAA;
-    obdFrame.data[7] = 0xAA;
-      // Accepts both pointers and references 
-    ESP32Can.writeFrame(obdFrame,0);  // timeout defaults to 1 ms
+	CanFrame obdFrame = { 0 };
+	obdFrame.identifier = 0x7DF; // Default OBD tester address;
+	obdFrame.extd = 0;
+	obdFrame.data_length_code = 8;
+	obdFrame.data[0] = 0x02;
+	obdFrame.data[1] = 0x01;    // Mode 1
+	obdFrame.data[2] = obdId;
+	obdFrame.data[3] = 0xAA;    // Best to use 0xAA (0b10101010) instead of 0
+	obdFrame.data[4] = 0xAA;    // CAN works better this way as it needs
+	obdFrame.data[5] = 0xAA;    // to avoid bit-stuffing
+	obdFrame.data[6] = 0xAA;
+	obdFrame.data[7] = 0xAA;
+	  // Accepts both pointers and references 
+	ESP32Can.writeFrame(obdFrame,0);  // timeout defaults to 1 ms
 }
 
 void canStart() {
-    ESP32Can.setPins(CAN_TX,CAN_RX);
-    ESP32Can.setSpeed(ESP32Can.convertSpeed(500));
-    ESP32Can.begin(); 
+	ESP32Can.setPins(CAN_TX,CAN_RX);
+	ESP32Can.setSpeed(ESP32Can.convertSpeed(500));
+	ESP32Can.begin(); 
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,11 @@
 #include <Arduino.h>
-#include <Wire.h>
 #include <ESP_Panel_Library.h>
-#include "CST816S.h"
+#include <ESP_IOExpander_Library.h>
 #include <Arduino_Helpers.h>
 #include <AH/Timing/MillisMicrosTimer.hpp>
 #include <ui.h>
 #include "obdHandler.h"
+#include "lvgl_9_2_port.h"
 
 #ifndef TFT_BL
   #define TFT_BL 5
@@ -18,27 +18,33 @@
 //Display buffer preparation
 #define TFT_HOR_RES 360
 #define TFT_VER_RES 360
-#define DRAW_BUF_SIZE (TFT_HOR_RES * TFT_VER_RES / 4 * (LV_COLOR_DEPTH / 8))
-uint32_t draw_buf[DRAW_BUF_SIZE];
+// #define DRAW_BUF_SIZE (TFT_HOR_RES * TFT_VER_RES / 4 * (LV_COLOR_DEPTH / 8))
+// uint32_t draw_buf[DRAW_BUF_SIZE];
+
+//ESP_Panel_Library declarations
+ESP_Panel *panel = nullptr;
+ESP_PanelLcd *lcd = nullptr;
+ESP_PanelTouch *touch = nullptr;
+ESP_IOExpander *expander = nullptr;
 
 // Touch initialisation
-#define TP_INT 4
-#define TP_SDA 1
-#define TP_SCL 3
-#define TP_RST -1
-CST816S touch(TP_SDA, TP_SCL, TP_RST, TP_INT);
-void touchRead(lv_indev_t *indev, lv_indev_data_t *data)
-{
+// #define TP_INT 4
+// #define TP_SDA 1
+// #define TP_SCL 3
+// #define TP_RST -1
+// CST816S touch(TP_SDA, TP_SCL, TP_RST, TP_INT);
+// void touchRead(lv_indev_t *indev, lv_indev_data_t *data)
+// {
   
-  if(touch.available()) {
-    data->state = LV_INDEV_STATE_PRESSED;
-    data->point.x = touch.data.x;
-    data->point.y = touch.data.y;
-  }
-  else {
-    data->state = LV_INDEV_STATE_RELEASED;
-  }
-}
+//   if(touch.available()) {
+//     data->state = LV_INDEV_STATE_PRESSED;
+//     data->point.x = touch.data.x;
+//     data->point.y = touch.data.y;
+//   }
+//   else {
+//     data->state = LV_INDEV_STATE_RELEASED;
+//   }
+// }
 
 //Timers
 #define TICKS 5
@@ -71,12 +77,12 @@ bool screenON = false;
 #ifdef TEST_GENERATOR
 void generateValues() {
   if(millis()>20000) {
-    intakeTemp_FR             =   true;
-    absBaroPressure_FR        =   true;
-    intakeManifoldPressure_FR =   true;
-    boostPressure_FR          =   true;
-    engineCoolantTemp_FR      =   true;
-    controlModuleVoltage_FR   =   true;
+	intakeTemp_FR             =   true;
+	absBaroPressure_FR        =   true;
+	intakeManifoldPressure_FR =   true;
+	boostPressure_FR          =   true;
+	engineCoolantTemp_FR      =   true;
+	controlModuleVoltage_FR   =   true;
   }
 
   intakeTemp    =   (int32_t)(127*(1+0.6*sin((2*PI/10000)*millis()))-40);
@@ -89,56 +95,56 @@ void generateValues() {
 #else
 void parseCANFrame() {
   if(ESP32Can.readFrame(rxFrame,0)) {
-    switch (rxFrame.identifier) {
-      case OBD_RESP_ID:
-        OBD_length  = rxFrame.data[0];
-        OBD_mode    = rxFrame.data[1];
-        OBD_command = rxFrame.data[2];
-        byteA       = rxFrame.data[3];
-        byteB       = rxFrame.data[4];
-        byteC       = rxFrame.data[5];
-        byteD       = rxFrame.data[6];
-        byteE       = rxFrame.data[7];
+	switch (rxFrame.identifier) {
+	  case OBD_RESP_ID:
+		OBD_length  = rxFrame.data[0];
+		OBD_mode    = rxFrame.data[1];
+		OBD_command = rxFrame.data[2];
+		byteA       = rxFrame.data[3];
+		byteB       = rxFrame.data[4];
+		byteC       = rxFrame.data[5];
+		byteD       = rxFrame.data[6];
+		byteE       = rxFrame.data[7];
 
-        if((OBD_mode-0x40)==0x01) { //Check we are in the correct mode
-          switch (OBD_command)  {
-            case 0x05: //Engine Coolant Temperature : engineCoolantTemp
-              engineCoolantTemp = ((int)byteA - 40);
-              engineCoolantTemp_FR = true;
-              break;
-            case 0x0B:
-              intakeManifoldPressure = (int)byteA;
-              intakeManifoldPressure_FR = true;
-              boostPressure_FR = absBaroPressure_FR && intakeManifoldPressure_FR;
-              break;
-            case 0x0F :
-              intakeTemp = ((int)byteA - 40);
-              intakeTemp_FR = true;
-              break;
-            case 0x33 : 
-              absBaroPressure = (int)byteA;
-              absBaroPressure_FR = true;
-              boostPressure_FR = absBaroPressure_FR && intakeManifoldPressure_FR;
-              break;
-            case 0x42 : //Control module voltage... unit is mV !
-              controlModuleVoltage = (256*(int)byteA + (int)byteB);
-              controlModuleVoltage_FR = true;
-              break;
-            default:
-              break;
-          }
-          boostPressure = intakeManifoldPressure - absBaroPressure;
-        }
-        lastConnected = millis();
-        break;
-      case 0x130: //Ignition and key detection
-        keyPresence = ((rxFrame.data[0] & 0xF0) == 0x40); //True if key present
-        ignitionOn  = ((rxFrame.data[0] & 0x0F) == 0x05); //True if ignition on
-        lastConnected = millis();
-        break;
-      default:
-        break;
-    }
+		if((OBD_mode-0x40)==0x01) { //Check we are in the correct mode
+		  switch (OBD_command)  {
+			case 0x05: //Engine Coolant Temperature : engineCoolantTemp
+			  engineCoolantTemp = ((int)byteA - 40);
+			  engineCoolantTemp_FR = true;
+			  break;
+			case 0x0B:
+			  intakeManifoldPressure = (int)byteA;
+			  intakeManifoldPressure_FR = true;
+			  boostPressure_FR = absBaroPressure_FR && intakeManifoldPressure_FR;
+			  break;
+			case 0x0F :
+			  intakeTemp = ((int)byteA - 40);
+			  intakeTemp_FR = true;
+			  break;
+			case 0x33 : 
+			  absBaroPressure = (int)byteA;
+			  absBaroPressure_FR = true;
+			  boostPressure_FR = absBaroPressure_FR && intakeManifoldPressure_FR;
+			  break;
+			case 0x42 : //Control module voltage... unit is mV !
+			  controlModuleVoltage = (256*(int)byteA + (int)byteB);
+			  controlModuleVoltage_FR = true;
+			  break;
+			default:
+			  break;
+		  }
+		  boostPressure = intakeManifoldPressure - absBaroPressure;
+		}
+		lastConnected = millis();
+		break;
+	  case 0x130: //Ignition and key detection
+		keyPresence = ((rxFrame.data[0] & 0xF0) == 0x40); //True if key present
+		ignitionOn  = ((rxFrame.data[0] & 0x0F) == 0x05); //True if ignition on
+		lastConnected = millis();
+		break;
+	  default:
+		break;
+	}
   }
 }
 #endif
@@ -146,91 +152,96 @@ void parseCANFrame() {
 #if LV_USE_LOG != 0
 void my_print( lv_log_level_t level, const char * buf )
 {
-    LV_UNUSED(level);
-    Serial.println(buf);
-    Serial.flush();
+	LV_UNUSED(level);
+	Serial.println(buf);
+	Serial.flush();
 }
 #endif
 
 void setup() {
   
-  //Initialise min-max indicators
-  engineCoolantTemp_max = -40;
-  engineCoolantTemp_min = 215;
+	//Initialise min-max indicators
+	engineCoolantTemp_max = -40;
+	engineCoolantTemp_min = 215;
 
-  boostPressure_max = -255;
-  boostPressure_min = 255;
+	boostPressure_max = -255;
+	boostPressure_min = 255;
 
-  intakeTemp_max = -40;
-  intakeTemp_min = 215;
+	intakeTemp_max = -40;
+	intakeTemp_min = 215;
 
-  controlModuleVoltage_max = 0;
-  controlModuleVoltage_min = 24000;
+	controlModuleVoltage_max = 0;
+	controlModuleVoltage_min = 24000;
 
-  //For Debug
-  Serial.begin(115200);
+	//For Debug
+	Serial.begin(115200);
 
-  //CAN start
-  canStart();
+	//CAN start
+	canStart();
 
-  #ifdef TEST_GENERATOR
-  generateValues();
-  #endif
+	#ifdef TEST_GENERATOR
+	generateValues();
+	#endif
 
-  //Touch startup
-  touch.begin();
+	//ESP Panel
+	panel = new ESP_Panel();
+	panel->init();
+	panel->begin();
+	lcd 		= 	panel->getLcd();
+	touch 		= 	panel->getTouch();
+	expander	=	panel->getExpander();
 
-  //ESP Panel
-  ESP_Panel *panel = new ESP_Panel();
-  panel->init();
+	//Now using LVGL_9_2port
+	Serial.println("Initialising LVGL Port");
+	lvgl_port_init(lcd,touch);
 
-  panel->begin();
+	// //LV startup sequence
+	// lv_init();
+	// #if LV_USE_LOG != 0
+	// 	lv_log_register_print_cb( my_print );
+	// #endif
+	// lv_display_t * disp;
+	// disp = lv_display_create(TFT_HOR_RES,TFT_VER_RES);
+	// //disp = lv_tft_espi_create(TFT_HOR_RES, TFT_VER_RES, draw_buf, sizeof(draw_buf));
+	// lv_indev_t *indev = lv_indev_create();
+	// lv_indev_set_type(indev, LV_INDEV_TYPE_POINTER);
+	// lv_indev_set_read_cb(indev,touchRead);
+	// lv_display_set_rotation(disp,LV_DISPLAY_ROTATION_270);
 
-  //LV startup sequence
-  lv_init();
-  #if LV_USE_LOG != 0
-    lv_log_register_print_cb( my_print );
-  #endif
-  lv_display_t * disp;
-  disp = lv_display_create(TFT_HOR_RES,TFT_VER_RES);
-  //disp = lv_tft_espi_create(TFT_HOR_RES, TFT_VER_RES, draw_buf, sizeof(draw_buf));
-  lv_indev_t *indev = lv_indev_create();
-  lv_indev_set_type(indev, LV_INDEV_TYPE_POINTER);
-  lv_indev_set_read_cb(indev,touchRead);
-  lv_display_set_rotation(disp,LV_DISPLAY_ROTATION_270);
-  //Draw screens
-  ui_init();
+	//Draw screens
+	Serial.println("Drawing UI");
+	ui_init();
 
-  //Debug
-  Serial.println( "Setup done" );
+	//Debug
+	Serial.println( "Setup done" );
 }
 
 void loop() {
 
   //Sends the OBD query only if the CAN is sending something as a keepalive first and if the key is present.
   if(OBDrequestDelay && canState && keyPresence) {
-    sendObdFrame(requestID);
-    //Move to the next requestID... would be better to use an enum list
-    switch (requestID)  {
-      case 0x05:
-        requestID = 0x0B;
-        break;
-      case 0x0B:
-        requestID = 0x0F;
-        break;
-      case 0x0F:
-        requestID = 0x33;
-        break;
-      case 0x33:
-        requestID = 0x42;
-        break;
-      case 0x42:
-        requestID = 0x05;
-        break;
-      default:
-        requestID = 0x05;
-        break;
-    }
+	sendObdFrame(requestID);
+	//Move to the next requestID... would be better to use an enum list
+	switch (requestID)  {
+	  case 0x05:
+		requestID = 0x0B;
+		break;
+	  case 0x0B:
+		requestID = 0x0F;
+		break;
+	  case 0x0F:
+		requestID = 0x33;
+		break;
+	  case 0x33:
+		requestID = 0x42;
+		break;
+	  case 0x42:
+		requestID = 0x05;
+		break;
+	  default:
+		requestID = 0x05;
+		break;
+	}
   }
   
   //Parse a CAN Frame if available
@@ -242,74 +253,74 @@ void loop() {
 
   //Update the canState in case of silent connection
   if(millis()-lastConnected>2000) {
-    canState = false;
-    }
+	canState = false;
+	}
   else {
-    canState = true;
+	canState = true;
   }
 
   //Refresh the items in the UI
   if(refreshValues) {
-    setCanState(canState);
-    setIgnitionState(ignitionOn);
-    setKeyPresence(keyPresence);
-    
-    //Coolant Screen
-    if(engineCoolantTemp_FR) {
-    updateCoolantScr(engineCoolantTemp);
-      if((engineCoolantTemp_min>engineCoolantTemp) || (engineCoolantTemp_max<engineCoolantTemp))  {
-        engineCoolantTemp_max = max(engineCoolantTemp_max,engineCoolantTemp);
-        engineCoolantTemp_min = min(engineCoolantTemp_min,engineCoolantTemp);
-        updateCoolantMinMax(engineCoolantTemp_min,engineCoolantTemp_max);
-      }
-    }
-    
-    //Boost Screen
-    if(boostPressure_FR) {
-      updateBoostScr(boostPressure);
-      if((boostPressure_min>boostPressure) || (boostPressure_max<boostPressure))  {
-        boostPressure_max = max(boostPressure_max,boostPressure);
-        boostPressure_min = min(boostPressure_min,boostPressure);
-        updateBoostMinMax(boostPressure_min,boostPressure_max);
-      }
-    }
+	setCanState(canState);
+	setIgnitionState(ignitionOn);
+	setKeyPresence(keyPresence);
+	
+	//Coolant Screen
+	if(engineCoolantTemp_FR) {
+	updateCoolantScr(engineCoolantTemp);
+	  if((engineCoolantTemp_min>engineCoolantTemp) || (engineCoolantTemp_max<engineCoolantTemp))  {
+		engineCoolantTemp_max = max(engineCoolantTemp_max,engineCoolantTemp);
+		engineCoolantTemp_min = min(engineCoolantTemp_min,engineCoolantTemp);
+		updateCoolantMinMax(engineCoolantTemp_min,engineCoolantTemp_max);
+	  }
+	}
+	
+	//Boost Screen
+	if(boostPressure_FR) {
+	  updateBoostScr(boostPressure);
+	  if((boostPressure_min>boostPressure) || (boostPressure_max<boostPressure))  {
+		boostPressure_max = max(boostPressure_max,boostPressure);
+		boostPressure_min = min(boostPressure_min,boostPressure);
+		updateBoostMinMax(boostPressure_min,boostPressure_max);
+	  }
+	}
 
-    //IAT Screen
-    if(intakeTemp_FR) {
-      updateIatScr(intakeTemp);
-      if((intakeTemp_min>intakeTemp) || (intakeTemp_max<intakeTemp))  {
-        intakeTemp_max = max(intakeTemp_max,intakeTemp);
-        intakeTemp_min = min(intakeTemp_min,intakeTemp);
-        updateIatMinMax(intakeTemp_min,intakeTemp_max);
-      }
-    }
+	//IAT Screen
+	if(intakeTemp_FR) {
+	  updateIatScr(intakeTemp);
+	  if((intakeTemp_min>intakeTemp) || (intakeTemp_max<intakeTemp))  {
+		intakeTemp_max = max(intakeTemp_max,intakeTemp);
+		intakeTemp_min = min(intakeTemp_min,intakeTemp);
+		updateIatMinMax(intakeTemp_min,intakeTemp_max);
+	  }
+	}
 
-    //Voltage Screen
-    if(controlModuleVoltage_FR) {
-      updateVoltageScr(controlModuleVoltage);
-      if((controlModuleVoltage_min>controlModuleVoltage) || (controlModuleVoltage_max<controlModuleVoltage))  {
-        controlModuleVoltage_max = max(controlModuleVoltage_max,controlModuleVoltage);
-        controlModuleVoltage_min = min(controlModuleVoltage_min,controlModuleVoltage);
-        updateVoltageMinMax(controlModuleVoltage_min,controlModuleVoltage_max);
-      }
-    }
+	//Voltage Screen
+	if(controlModuleVoltage_FR) {
+	  updateVoltageScr(controlModuleVoltage);
+	  if((controlModuleVoltage_min>controlModuleVoltage) || (controlModuleVoltage_max<controlModuleVoltage))  {
+		controlModuleVoltage_max = max(controlModuleVoltage_max,controlModuleVoltage);
+		controlModuleVoltage_min = min(controlModuleVoltage_min,controlModuleVoltage);
+		updateVoltageMinMax(controlModuleVoltage_min,controlModuleVoltage_max);
+	  }
+	}
   }
   
   //Loop LVGL
   if(tickerLVGL) {
-    lv_task_handler();
-    lv_tick_inc(TICKS);
+	lv_task_handler();
+	lv_tick_inc(TICKS);
   }
 
   //Initial screenON
   if(!screenON) {
-    uint8_t tempbrightness = 0;
-    while (tempbrightness<BRIGHTNESS) {
-      analogWrite(TFT_BL,tempbrightness);
-      delay(3);
-      tempbrightness++;
-    }
-    //analogWrite(TFT_BL,BRIGHTNESS);
-    screenON = true;
+	uint8_t tempbrightness = 0;
+	while (tempbrightness<BRIGHTNESS) {
+	  analogWrite(TFT_BL,tempbrightness);
+	  delay(3);
+	  tempbrightness++;
+	}
+	//analogWrite(TFT_BL,BRIGHTNESS);
+	screenON = true;
   }
 }
