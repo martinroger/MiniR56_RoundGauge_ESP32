ac70352eceb750172d5a9d4be899a07a08174e74
Martin <martin.roger@me.com>
2024-10-27 14:54:02 +0100

    Introducing vars.c

---
+++ lib/ui/vars.c
@@ -0,0 +1,13 @@
+#include "vars.h"
+
+int32_t engineCoolantTemp_max = 0;
+int32_t engineCoolantTemp_min = 0;
+
+int32_t boostPressure_max = 0;
+int32_t boostPressure_min = 0;
+
+int32_t intakeTemp_max = 0;
+int32_t intakeTemp_min = 0;
+
+int32_t controlModuleVoltage_max = 0;
+int32_t controlModuleVoltage_min = 0;
