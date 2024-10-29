65c57e29b3108ff60619af0777f99fe1d43916b9
Martin <martin.roger@me.com>
2024-10-29 11:30:16 +0100

    Direct mode configuration

--- include/lvgl_port.h
+++ include/lvgl_port.h
@@ -46,12 +46,36 @@ void displayInit(void)
     lv_tick_set_cb(tick);
 
     //Buffers
-    LV_ATTRIBUTE_MEM_ALIGN
-    static uint8_t buf1[TFT_HOR_RES * 16 * BYTE_PER_PIXEL];
+    // LV_ATTRIBUTE_MEM_ALIGN
+    // static uint8_t buf1[TFT_HOR_RES * TFT_VER_RES * BYTE_PER_PIXEL/2];
+    
+    // LV_ATTRIBUTE_MEM_ALIGN
+    // static uint8_t buf2[TFT_HOR_RES * TFT_VER_RES * BYTE_PER_PIXEL/2];
 
-    LV_ATTRIBUTE_MEM_ALIGN
-    static uint8_t buf2[TFT_HOR_RES * 16 * BYTE_PER_PIXEL];
-    lv_display_set_buffers(disp,buf1,buf2,sizeof(buf1),LV_DISPLAY_RENDER_MODE_PARTIAL);
+    //Partial rendering
+    // uint32_t buffer_size = TFT_HOR_RES * 120 *lv_color_format_get_size(lv_display_get_color_format(disp));
+    // void *buf[2] = {nullptr};
+    // buf[0] = heap_caps_malloc(buffer_size, MALLOC_CAP_INTERNAL | MALLOC_CAP_8BIT);
+    // assert(buf[0]);
+    // buf[1] = heap_caps_malloc(buffer_size, MALLOC_CAP_INTERNAL | MALLOC_CAP_8BIT);
+    // assert(buf[1]);
+    // lv_display_set_buffers(disp,buf[0],buf[1],buffer_size,LV_DISPLAY_RENDER_MODE_PARTIAL);
+
+    //Direct or Full mode
+    // uint32_t buffer_size = TFT_HOR_RES * TFT_VER_RES*lv_color_format_get_size(lv_display_get_color_format(disp));
+    // void *buf[2] = {nullptr};
+    // buf[0] = heap_caps_malloc(buffer_size, MALLOC_CAP_SPIRAM);
+    // assert(buf[0]);
+    // buf[1] = heap_caps_malloc(buffer_size, MALLOC_CAP_SPIRAM);
+    // assert(buf[1]);
+    // lv_display_set_buffers(disp,buf[0],buf[1],buffer_size,LV_DISPLAY_RENDER_MODE_FULL);
+
+    //Third method for RGB screens
+    uint32_t buffer_size = TFT_HOR_RES * TFT_VER_RES * lv_color_format_get_size(lv_display_get_color_format(disp));
+    void *buf[2] = {nullptr};
+    buf[0] = lcd->getRgbBufferByIndex(0);
+    buf[1] = lcd->getRgbBufferByIndex(1);
+    lv_display_set_buffers(disp,buf[0],buf[1],buffer_size,LV_DISPLAY_RENDER_MODE_DIRECT);
 
     //Prepare the Touch pointer
     lv_indev_t *indev = lv_indev_create();
